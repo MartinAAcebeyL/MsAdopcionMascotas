6fcf5c2e8d9ce444789b138cd8d8c8766935282b
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2024-05-25 11:01:26 -0400

    feat: finish command to create fake pets

--- adoption_ms/apps/pets/management/commands/GetData.py
+++ adoption_ms/apps/pets/management/commands/GetData.py
@@ -1,13 +1,28 @@
-from django.core.management.base import BaseCommand, CommandError
-
+from django.core.management.base import BaseCommand
 
 from .usecases import FakePet
+from apps.pets.models import Pet
 
 
 class Command(BaseCommand):
-    help = "Get data from the Dog API"
+    help = "Save fake data in pets collection"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "amount",
+            nargs="?",
+            type=int,
+            default=5,
+            help="Number of pets to create",
+        )
 
     def handle(self, *args, **options):
-        fake_pet = FakePet()
-        print(fake_pet.fake_a_pet())
-        # self.stdout.write(self.style.SUCCESS("Data saved successfully"))
+        try:
+            pet_model = Pet()
+            amount = options.get("amount", 5)
+            fake_pet = FakePet()
+            for _ in range(amount):
+                pet_model.save_a_pet(fake_pet.fake_a_pet())
+            self.stdout.write(self.style.SUCCESS("Data saved successfully"))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Something was wrong: {e}"))
--- adoption_ms/apps/pets/models.py
+++ adoption_ms/apps/pets/models.py
@@ -1,8 +1,21 @@
+import logging
 from frameworks import mongo_client
 
+
 class Pet:
+    def __init__(self) -> None:
+        self.pets_collection = mongo_client.get_collection("pets")
+
     @classmethod
     def get_all_users(cls):
         users_collection = mongo_client.get_collection("users")
         all_users = list(users_collection.find({}))
         return all_users
+
+    def save_a_pet(self, pet: dict):
+        try:
+            new_pet = self.pets_collection.insert_one(pet)
+            return new_pet.inserted_id
+        except Exception as e:
+            logging.error(f"An error occurred while saving the pet: {e}")
+            return None
