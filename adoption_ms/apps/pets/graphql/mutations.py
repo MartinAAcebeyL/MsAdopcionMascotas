bdfa8b948ebc72e79e89d94d8c5ff84d02738fac
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2024-07-16 22:00:04 -0400

    feat: Add support for updating a pet in GraphQL mutation

--- adoption_ms/apps/pets/graphql/mutations.py
+++ adoption_ms/apps/pets/graphql/mutations.py
@@ -1,6 +1,7 @@
 from graphene import ObjectType
-from .types import CreatePetMutation
+from .types import CreatePetMutation, UpdatePetMutation
 
 
 class Mutation(ObjectType):
     create_pet = CreatePetMutation.Field()
+    update_pet = UpdatePetMutation.Field()
--- adoption_ms/apps/pets/graphql/types.py
+++ adoption_ms/apps/pets/graphql/types.py
@@ -7,7 +7,7 @@ from django.core.exceptions import PermissionDenied
 from .enum import SexEnum, SizeEnum, StatusEnum
 from apps.pets.db.entity import PetEntity
 from apps.pets.db.models import Pet
-from apps.pets.serializers import CreatePet
+from apps.pets.serializers import CreatePet, GetUpdatePet
 from adoption_ms.authentication import GoogleOAuth2Authentication
 
 
@@ -30,18 +30,32 @@ class PetConnection(relay.Connection):
 
 
 # Mutations
+class UpdateResponseType(graphene.ObjectType):
+    message = graphene.String()
+    id = graphene.ID(required=True)
+    name = graphene.String()
+    history = graphene.String()
+    age_value = graphene.Int()
+    age_time = graphene.String()
+    breed = graphene.String()
+    type = graphene.String()
+    city = graphene.String()
+    size = SizeEnum()
+    status = StatusEnum()
+
+
 class CreatePetMutation(Mutation):
     class Arguments:
-        name = graphene.String()
+        name = graphene.String(required=True)
         history = graphene.String()
-        age_value = graphene.Int()
-        age_time = graphene.String()
+        age_value = graphene.Int(required=True)
+        age_time = graphene.String(required=True)
         breed = graphene.String()
         type = graphene.String()
-        city = graphene.String()
+        city = graphene.String(required=True)
         size = SizeEnum()
         sex = SexEnum()
-        status = StatusEnum()
+        status = StatusEnum(default_value="available")
 
     pet = graphene.Field(PetType)
 
@@ -61,4 +75,55 @@ class CreatePetMutation(Mutation):
             return CreatePetMutation(pet=PetEntity(**new_pet))
         except Exception as e:
             logging.error(f"An error occurred while creating a pet: {e}")
+            raise e
+
+
+class UpdatePetMutation(Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        name = graphene.String()
+        history = graphene.String()
+        age_value = graphene.Int()
+        age_time = graphene.String()
+        breed = graphene.String()
+        type = graphene.String()
+        city = graphene.String()
+        size = SizeEnum()
+        status = StatusEnum()
+
+    pet = graphene.Field(UpdateResponseType)
+
+    @classmethod
+    def mutate(cls, _, info, **kwargs):
+        try:
+
+            auth = GoogleOAuth2Authentication()
+            user, _ = auth.authenticate(info.context)
+            if not user:
+                raise PermissionDenied("You must be logged to update a pet")
+            serializer = GetUpdatePet(data=kwargs)
+            serializer.is_valid(raise_exception=True)
+
+            pet_model = Pet()
+            pet_id = kwargs.get("id")
+            modified_count, matched_count = pet_model.mofify_a_pet(
+                serializer.validated_data, pet_id
+            )
+            message = ""
+            response = {
+                "id": str(pet_id),
+                **serializer.validated_data,
+            }
+
+            if modified_count == 0 and matched_count == 1:
+                message = f"To pet with id {pet_id} the data you send is the same as the saved ones"
+            elif matched_count == 0:
+                message = f"pet with id {pet_id} was not found"
+            elif modified_count == 1:
+                message = f"pet with id {pet_id} was updated"
+
+            response["message"] = message
+            return UpdatePetMutation(pet=UpdateResponseType(**response))
+        except Exception as e:
+            logging.error(f"An error occurred while updating a pet: {e}")
             raise e
